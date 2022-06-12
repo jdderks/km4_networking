2d9c6c55f17600b257c5318b250297fb22229bde
Joris Desktop <j_d_derks@hotmail.com>
2022-06-13 00:04:43 +0200

    add scoreboard integration

--- Assets/Scripts/Scoreboard.cs
+++ Assets/Scripts/Scoreboard.cs
@@ -1,19 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using System;
 
 public class Scoreboard : MonoBehaviour
 {
     [SerializeField] private List<GameObject> scoreObjects;
+    private string[] importedScoreTexts;
 
+    [ContextMenu("Lastweekupdate")]
+    private void LastWeekUpdate()
+    {
+        StartCoroutine(GetLastWeekInfo());
+    }
 
     [ContextMenu("SetScoresAllTime")]
     private void SetScoresAllTime()
     {
+        int number = 0;
         for (int i = 0; i < scoreObjects.Count; i++)
         {
+            //Set number
             Scores scores = scoreObjects[i].GetComponent<Scores>();
-            scores.NumberText.text = i.ToString();
+            number++;
+            scores.NumberText.text = number.ToString();
+
+
+            //Set Username
+
         }
     }
 
@@ -21,4 +36,39 @@ public class Scoreboard : MonoBehaviour
     {
 
     }
+
+    private IEnumerator GetLastWeekInfo()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("https://studentdav.hku.nl/~joris.derks/networking/get_bestinlastweek.php");
+        yield return www.SendWebRequest();
+
+        if (www.result == UnityWebRequest.Result.ConnectionError)
+        {
+            Debug.LogError("Couldn't collect info.");
+        }
+        else
+        {
+            if (www.downloadHandler.text != "Fail")
+            {
+                string json = www.downloadHandler.text;
+                string[] jsonArray = json.Split(new string[] { "[", "]", "{", "}", ":", ",", "name", "score", "time", '"'.ToString()}, StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < jsonArray.Length; i+=3)
+                {
+                    Debug.Log(jsonArray[i]);
+                    HighScore high = new HighScore();
+                    high.name = jsonArray[i];
+                    high.score = int.Parse(jsonArray[i+1]);
+                    high.date = long.Parse(jsonArray[i+2]);
+                }
+            }
+        }
+    }
+}
+
+[Serializable]
+public class HighScore
+{
+    public string name;
+    public int score;
+    public long date;
 }
